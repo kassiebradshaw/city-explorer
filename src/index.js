f503d9c6bd14ac51f6ccb68d293398ea72cc301c
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2021-04-06 22:30:29 -0700

    jquery installed per deploy log

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import Axios from 'axios';
-import Bootstrap from 'bootstrap';
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
